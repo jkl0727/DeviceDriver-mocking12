b8606857bd8d372864b318734af3d2c2b435ec8d
jungwang <jklkwang0727@gmail.com>
2023-07-07 11:06:41 +0900

    [feature] add driver read & mocking test

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,11 +1,28 @@
+#include <windows.h>
+#include <stdexcept>
 #include "DeviceDriver.h"
+using namespace std;
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
+    int readResult[5] = { 0, };
+    for (int i = 0; i < 5; ++i)
+    {
+        readResult[i] = m_hardware->read(address);
+    	Sleep(200);
+    }
+
+    for (int i = 1; i < 5; ++i)
+    {
+	    if (readResult[0] != readResult[i])
+	    {
+            throw logic_error("values are wrong");
+	    }
+    }
+
     return (int)(m_hardware->read(address));
 }
 
